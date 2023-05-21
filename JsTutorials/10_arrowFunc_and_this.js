360babc7cc2a14d38365c0250982a924617c9886
Ganesh R G <gurkhudeganesh@gmail.com>
2023-05-21 20:17:30 +0530

    Object

--- JsTutorials/10_arrowFunc_and_this.js
+++ JsTutorials/10_arrowFunc_and_this.js
@@ -12,4 +12,5 @@ getName.call(p);   //--> assigned this of getName to object p
 getAge.call(p)
 
 
-// in arrow function {this} refers to global object and in browser to window object
+// in arrow function {this} (not METHOD -property of object) refers to global object and in browser to window object always even in STRICT mode
+// in method (not arrow function )(of object) this always will refer to the one which calls the method... not necessary to the object of which the method is property...
